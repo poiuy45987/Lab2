5a1efa9f6517e0e8dcbe697d1d9ce608202abea0
CheolHee <poiuy45987@snu.ac.kr>
2024-06-07 10:19:45 +0000

    main.cpp modified

--- main.cpp
+++ main.cpp
@@ -7,16 +7,26 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			cout << vec[i] << " ";
+		}
+		cout << endl;
+	}
+	else if (cnt == 0) {
+		for (vec[cnt] = 1; vec[cnt] <= N; vec[cnt]++) {
+			func(cnt + 1);
+		}
+	}
+	else {
+		for (vec[cnt] = vec[cnt - 1] + 1; vec[cnt] <= N; vec[cnt]++) {
+			func(cnt + 1);
+		}
+	}
 }
 
 int main() {
    cin >> N >> M;
+   vec.assign(M, 0);
    func(0);
 }
